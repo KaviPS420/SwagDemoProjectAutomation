284835d8e748815d19725f1235a22a4f3585c872
KaviPS420 <kavithapsmail@gmail.com.com>
2025-04-29 16:44:55 -0700

    updating LoginpageTest

--- src/test/java/com/qa/swaglabs/tests/LoginPageTest.java
+++ src/test/java/com/qa/swaglabs/tests/LoginPageTest.java
@@ -8,11 +8,22 @@ import com.qa.swaglabs.pages.LoginPage;
 
 public class LoginPageTest extends BaseTest{
 	
-	@Test
+	@Test(priority=1)
    public void verifyLoginPageTitle() {
 	String actTitle = loginpage.getLoginpageTitle();
 	Assert.assertEquals(actTitle, "Swag Labs");
 		
    }
+	@Test(priority=2)
+	public void verifyLoginpageUrl() {
+		String url=loginpage.getLoginPageURl();
+		Assert.assertEquals(url, "https://www.saucedemo.com/");
+		
+	}
+	@Test(priority=3)
+	public void verifyDoLogin() {
+		String land = loginpage.doLogin("standard_user", "secret_sauce");
+		Assert.assertEquals(land, "Swag Labs") ;		
+	}
 	
 }
