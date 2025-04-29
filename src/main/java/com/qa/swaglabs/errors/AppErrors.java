48766990ade5489c07833d40e975caaaef3047c7
KaviPS420 <kavithapsmail@gmail.com.com>
2025-04-28 18:12:17 -0700

    Pushing my updated DriverFac and AppErrors

--- src/main/java/com/qa/swaglabs/errors/AppErrors.java
+++ src/main/java/com/qa/swaglabs/errors/AppErrors.java
@@ -1,5 +1,6 @@
 package com.qa.swaglabs.errors;
 
 public class AppErrors {
+	public static final String BROWSER_NOT_FOUND ="***** BROWSER NOT FOUND*****";
 
 }
--- src/main/java/com/qa/swaglabs/factory/DriverFactory.java
+++ src/main/java/com/qa/swaglabs/factory/DriverFactory.java
@@ -3,9 +3,60 @@ package com.qa.swaglabs.factory;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import com.qa.swaglabs.errors.AppErrors;
+import com.qa.swaglabs.exceptions.BrowserExceptions;
 
 public class DriverFactory {
 
 	WebDriver driver;
 	Properties prop;
+	
+	public void intiDriver(String browserName) {
+		//Cross browser logic 
+		
+	}
+	public void initPro() {
+		//cross property or Env logic 
+		
+		String browserName = prop.getProperty("browser");
+		System.out.println("browser name is : " + browserName);
+
+		switch (browserName.toLowerCase().trim()) {
+		case "chrome":
+			driver = new ChromeDriver();
+			break;
+		case "firefox":
+			driver = new FirefoxDriver();
+			break;
+		case "edge":
+			driver = new EdgeDriver();
+			break;
+		case "safari":
+			driver = new SafariDriver();
+			break;
+
+		default:
+			System.out.println("plz pass the right browser name..." + browserName);
+			throw new BrowserExceptions(AppErrors.BROWSER_NOT_FOUND);
+			
+		}
+
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+	
+
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 }
