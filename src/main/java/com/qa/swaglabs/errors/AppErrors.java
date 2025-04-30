3576f09860cc9ea3a0e27562b2ca11fe3bf0e07f
KaviPS420 <kavithapsmail@gmail.com.com>
2025-04-29 17:14:35 -0700

    AppError page update

--- src/main/java/com/qa/swaglabs/errors/AppErrors.java
+++ src/main/java/com/qa/swaglabs/errors/AppErrors.java
@@ -1,6 +1,8 @@
 package com.qa.swaglabs.errors;
 
 public class AppErrors {
-	public static final String BROWSER_NOT_FOUND ="***** BROWSER NOT FOUND*****";
+	public static final String BROWSER_NOT_FOUND ="***** BROWSER NOT FOUND *****";
+	public static final String TITLE_NOT_FOUND = "***** TITLE NOT FOUND *****";
+	public static final String URL_NOT_FOUND = "***** URL NOT FOUND *****";
 
 }
--- src/main/java/com/qa/swaglabs/pages/inventoryPage.java
+++ src/main/java/com/qa/swaglabs/pages/inventoryPage.java
@@ -1,5 +1,13 @@
 package com.qa.swaglabs.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class inventoryPage {
+	WebDriver driver;
+	
+	//Creating Page object using By locater
+	By menu = By.id("react-burger-menu-btn");
+	
 
 }
